220aa3627b56e6e33472a4e98e74b6d2f5ae33c1
Leandro Felix <leandro123felix123@gmail.com>
2023-07-13 10:49:07 -0300

    Adicionando resposividade do navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,10 +3,11 @@ import { useContext } from "react";
 import Switch from "react-switch";
 
 import { GoHome, GoSignIn, GoSignOut } from "react-icons/go";
-import { FaRegIdCard, FaFileSignature, FaUserMd } from "react-icons/fa";
-import { IoTicket } from "react-icons/io5";
+import { FaRegIdCard, FaUserMd } from "react-icons/fa";
+// import { FaFileSignature } from "react-icons/fa";
+// import { IoTicket } from "react-icons/io5";
 import { GiMedicinePills } from "react-icons/gi";
-import { TbUserHeart } from "react-icons/tb";
+// import { TbUserHeart } from "react-icons/tb";
 import { RiUserVoiceLine } from "react-icons/ri";
 
 import { FiSun } from "react-icons/fi";
@@ -16,13 +17,13 @@ import styles from "../styles/Navbar.module.css";
 import { DarkModeContext } from "../../context/DarkModeContext";
 import { AuthPacienteContext } from "../../context/AuthContext";
 import { AuthMedContext } from "../../context/AuthMedContext";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 import Logo from "../../assets/img/medical-team.png";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function Navbar() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
   const { user, isAuthenticated, signOut } = useContext(AuthPacienteContext);
   const { userDoctor, doctorAuthenticated, signOutDoctor } =
@@ -31,16 +32,14 @@ function Navbar() {
 
   const makeLogout = () => {
     signOut();
-   // navigate("/login");
+    // navigate("/login");
     window.location.reload();
-
   };
 
   const makeDoctorLogout = () => {
     signOutDoctor();
-   // navigate("/login");
-   window.location.reload();
-
+    // navigate("/login");
+    window.location.reload();
   };
 
   console.log(userDoctor);
@@ -57,106 +56,136 @@ function Navbar() {
   console.log(doctorAuthenticated);
 
   return (
-    <nav className={navbarClassName}>
-      <div className={styles.navbar_logo}>
-        <img src={Logo} alt="logo" />
-        <h2>
-          <Link to={"/"}>MedSaúde</Link>
-        </h2>
-      </div>
-      <ul>
-        {user ? (
-          <>
-            <li>
-              <GoHome size={22} color="white" />
-              <Link to="/">Home</Link>
-            </li>
+    <nav className={`navbar ${navbarClassName} navbar-expand-lg`}>
+      <Container>
+        <Row className="align-itens-center">
+          <Col md={6}>
+            <div className={styles.navbar_logo}>
+              <img src={Logo} alt="logo" />
+              <h2>
+                <Link to={"/"}>MedSaúde</Link>
+              </h2>
+            </div>
+          </Col>
 
-            <li>
-              <FaRegIdCard size={22} color="white" />
-              <Link to="/userProfile">Perfil</Link>
-            </li>
-{/* 
+          <Col md={6}>
+            <ul className="navbar-nav ml-auto">
+              {user ? (
+                <>
+                  <li className="nav-item">
+                    <GoHome size={22} color="white" />
+                    <Link className="nav-link" to="/">
+                      Home
+                    </Link>
+                  </li>
+
+                  <li className="nav-item">
+                    <FaRegIdCard size={22} color="white" />
+                    <Link className="nav-link" to="/userProfile">
+                      Perfil
+                    </Link>
+                  </li>
+                  {/* 
             <li>
               <GiMedicinePills size={22} color="white" />
               <Link to="/medication">Medicação</Link>
             </li> */}
 
-            <li>
-              <RiUserVoiceLine size={22} color="white" />
-              <Link to="/consultation">Consultas</Link>
-            </li>
+                  <li className="nav-item">
+                    <RiUserVoiceLine size={22} color="white" />
+                    <Link className="nav-link" to="/consultation">
+                      Consultas
+                    </Link>
+                  </li>
 
-            {/* <li>
+                  {/* <li>
               <IoTicket size={22} color="white" />
               <Link to="/tickets">Fichas</Link>
             </li> */}
 
-            <li>
-              <FaUserMd size={22} color="white" />
-              <Link to="/consulta/new">Plantões</Link>
-            </li>
+                  <li className="nav-item">
+                    <FaUserMd size={22} color="white" />
+                    <Link className="nav-link" to="/consulta/new">
+                      Plantões
+                    </Link>
+                  </li>
 
-            <li>
-              <Button onClick={makeLogout}>
-                <GoSignOut size={22} color="white" />
-                Sair
-              </Button>
-            </li>
-          </>
-        ) : userDoctor ? (
-          <>
-            <li>
-              <GoHome size={22} color="white" />
-              <Link to="/doctor">Home</Link>
-            </li>
+                  <li className="nav-item">
+                    <Button onClick={makeLogout} className="btn btn-primary">
+                      <GoSignOut size={22} color="white" />
+                      Sair
+                    </Button>
+                  </li>
+                </>
+              ) : userDoctor ? (
+                <>
+                  <li className="nav-item">
+                    <GoHome size={22} color="white" />
+                    <Link className="nav-link" to="/doctor">
+                      Home
+                    </Link>
+                  </li>
 
-            <li>
-              <FaRegIdCard size={22} color="white" />
-              <Link to="/profileDoctor">Perfil</Link>
-            </li>
+                  <li className="nav-item">
+                    <FaRegIdCard size={22} color="white" />
+                    <Link className="nav-link" to="/profileDoctor">
+                      Perfil
+                    </Link>
+                  </li>
 
-            {/* <li>
+                  {/* <li>
               <RiUserVoiceLine size={22} color="white" />
               <Link to="/doctorConsultations">Consultas</Link>
             </li> */}
 
-            <li>
-              <FaUserMd size={22} color="white" />
-              <Link to="/doctorShifts">Plantões</Link>
-            </li>
+                  <li className="nav-item">
+                    <FaUserMd size={22} color="white" />
+                    <Link className="nav-link" to="/doctorShifts">
+                      Plantões
+                    </Link>
+                  </li>
 
-            {/* <li>
+                  {/* <li>
               <FaFileSignature size={22} color="white" />
               <Link to="/prescriptions">Prescrições</Link>
             </li> */}
 
-            <li>
-              <GiMedicinePills size={22} color="white" />
-              <Link to="/doctorMedications">Medicações</Link>
-            </li>
+                  <li className="nav-item">
+                    <GiMedicinePills size={22} color="white" />
+                    <Link className="nav-link" to="/doctorMedications">
+                      Medicações
+                    </Link>
+                  </li>
 
-            {/* <li>
+                  {/* <li>
               <TbUserHeart size={22} color="white" />
               <Link to="/patientStatus">Status do Paciente</Link>
             </li> */}
 
-            <li>
-              <Button onClick={makeDoctorLogout}>
-                <GoSignOut size={22} color="white" />
-                Sair
-              </Button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <GoSignIn size={22} color="white" />
-              <Link to="/login">Entrar</Link>
-            </li>
-          </>
-        )}
-      </ul>
+                  <li className="nav-item">
+                    <Button
+                      onClick={makeDoctorLogout}
+                      className="btn btn-prrimary"
+                    >
+                      <GoSignOut size={22} color="white" />
+                      Sair
+                    </Button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <GoSignIn size={22} color="white" />
+                    <Link className="nav-link" to="/login">
+                      Entrar
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
 
       <div className={styles.darkModeToggle}>
         <div className={styles.iconLeft}>
@@ -177,31 +206,6 @@ function Navbar() {
           <IoMoon size={26} color={switchOffColor} />
         </div>
       </div>
-      {/* 
-      <div className={styles.userTypeToggle}>
-        <div className={styles.radioGroup}>
-          <input
-            type="radio"
-            id="paciente"
-            name="userType"
-            value="paciente"
-            checked={userType === "paciente"}
-            onChange={() => setUserType("paciente")}
-          />
-          <label htmlFor="paciente">Paciente</label>
-        </div>
-        <div className={styles.radioGroup}>
-          <input
-            type="radio"
-            id="medico"
-            name="userType"
-            value="médico"
-            checked={userType === "médico"}
-            onChange={() => setUserType("médico")}
-          />
-          <label htmlFor="medico">Médico</label>
-        </div>
-      </div> */}
     </nav>
   );
 }
